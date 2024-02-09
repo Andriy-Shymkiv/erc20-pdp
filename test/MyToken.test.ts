fb0787d3bb4caa9c0b93acea0b564f266efb193c
Andriy-Shymkiv <winamondezik@gmail.com>
2024-02-09 16:42:38 +0200

    feat: add tests & coverage & update package.json

---
+++ test/MyToken.test.ts
@@ -0,0 +1,31 @@
+import { ethers } from 'hardhat';
+import { expect } from 'chai';
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
+
+describe('MyToken', () => {
+  async function deploy() {
+    const [deployer] = await ethers.getSigners();
+    const contract = await ethers.deployContract('MyToken');
+    return { contract, deployer };
+  }
+
+  it('is deployed', async () => {
+    await ethers.getSigners();
+    await ethers.deployContract('MyToken');
+  });
+
+  it('has MTK symbol', async () => {
+    const { contract } = await loadFixture(deploy);
+    expect(await contract.symbol()).to.equal('MTK');
+  });
+
+  it('has MyToken name', async () => {
+    const { contract } = await loadFixture(deploy);
+    expect(await contract.name()).to.equal('MyToken');
+  });
+
+  it('has 18 decimals', async () => {
+    const { contract } = await loadFixture(deploy);
+    expect(await contract.decimals()).to.equal(18);
+  });
+});
